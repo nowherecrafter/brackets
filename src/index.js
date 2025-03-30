220251841cdb8f2607733bfd709958aa5a5ac127
nowherecrafter <110341990+nowherecrafter@users.noreply.github.com>
2025-03-30 19:05:05 +0200

    Fixed the same symbol handling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = function check(str, bracketsConfig) {
   const stackOpen = [];
-
   const allBrackets = new Set();
   const closingBrackets = new Set();
   const bracketDictionary = new Map();
@@ -16,23 +15,26 @@ module.exports = function check(str, bracketsConfig) {
 
     if (bracketDictionary.has(char)) {
       const closingBracket = bracketDictionary.get(char);
-
+      
       if (closingBracket === char) {
         if (stackOpen.length > 0 && stackOpen[stackOpen.length - 1] === char) {
           stackOpen.pop();
+        } else {
+          stackOpen.push(char);
         }
-      } else {
+      } 
+
+      else {
         stackOpen.push(char);
       }
     }
 
     else if (closingBrackets.has(char)) {
-      if (stackOpen.length === 0) return false;
-      const lastOpen = stackpop();
-      if (bracketDictionary.get(lastOpen) !== char) return false;
-
+      if (stackOpen.length === 0) return false; 
+      const lastOpen = stackOpen.pop();
+      if (bracketDictionary.get(lastOpen) !== char) return false; 
     }
   }
 
   return stackOpen.length === 0;
-}
+};
