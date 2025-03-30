dc12f4ffc0af025ca0e8f6b449ee35e913e54945
nowherecrafter <110341990+nowherecrafter@users.noreply.github.com>
2025-03-30 18:40:09 +0200

    Update index.js
    Solution written

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stackOpen = [];
+
+  const allBrackets = new Set();
+  const closingBrackets = new Set();
+  const bracketDictionary = new Map();
+  
+  for (const [open, close] of bracketsConfig) {
+    bracketDictionary.set(open, close);
+    closingBrackets.add(close);
+    allBrackets.add(open).add(close);
+  }
+
+  for (const char of str) {
+    if (!allBrackets.has(char)) continue;
+
+    if (bracketDictionary.has(char)) {
+      const closingBracket = bracketDictionary.get(char);
+
+      if (closingBracket === char) {
+        if (stackOpen.length > 0 && stackOpen[stackOpen.length - 1] === char) {
+          stackOpen.pop();
+        }
+      } else {
+        stackOpen.push(char);
+      }
+    }
+
+    else if (closingBrackets.has(char)) {
+      if (stackOpen.length === 0) return false;
+      const lastOpen = stackpop();
+      if (bracketDictionary.get(lastOpen) !== char) return false;
+
+    }
+  }
+
+  return stackOpen.length === 0;
 }
